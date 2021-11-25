88dd0ce7a20782709d91d9dc8973362917a3d730
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-11-25 16:28:40 +0100

    refine query

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ async fn fetch_groups(
     client: reqwest::Client,
     token: Result<String, VarError>,
 ) -> Result<Vec<Group>> {
-    let mut req = client.get("https://gitlab.ppro.com/api/v4/groups?all_available&top_level");
+    let mut req = client
+        .get("https://gitlab.ppro.com/api/v4/groups?statistics=false&top_level=&with_custom_attributes=false&all_available=true&top_level&order_by=id&sort=asc&pagination=keyset&per_page=100"); // TODO: pagination
     if let Ok(token) = token {
         req = req.header("PRIVATE-TOKEN", token);
     }
