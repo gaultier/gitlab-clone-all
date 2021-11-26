f7ed8abb337c912be8bc46d473443caa97c4c281
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-11-26 10:01:14 +0100

    Clone over ssh

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{Context, Result};
 use git2::Repository;
+use git2::{Cred, Error, RemoteCallbacks};
 use reqwest::header::HeaderMap;
 use reqwest::header::HeaderValue;
 use reqwest::Client;
@@ -70,7 +71,27 @@ async fn clone_projects(mut rx: Receiver<Project>) {
     while let Some(project) = rx.recv().await {
         let path = root_dir_path.join(&project.path_with_namespace);
         println!("Received project {:?} fs_path={:?}", &project, &path);
-        match Repository::clone(&project.http_url_to_repo, path) {
+
+        let mut callbacks = RemoteCallbacks::new();
+        callbacks.credentials(|_url, username_from_url, _allowed_types| {
+            Cred::ssh_key(
+                username_from_url.unwrap(),
+                None,
+                std::path::Path::new(&format!(
+                    "{}/.ssh/id_rsa_gitlab",
+                    std::env::var("HOME").unwrap()
+                )),
+                None,
+            )
+        });
+        // Prepare fetch options.
+        let mut fo = git2::FetchOptions::new();
+        fo.remote_callbacks(callbacks);
+
+        // Prepare builder.
+        let mut builder = git2::build::RepoBuilder::new();
+        builder.fetch_options(fo);
+        match builder.clone(&project.http_url_to_repo, &path) {
             Ok(_repo) => {
                 println!("Cloned project={:?}", &project);
             }
