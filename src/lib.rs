2ee5fb9a86136024401a5b2e5aad3f07e28e0c7f
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-12-21 16:09:29 +0100

    Add git.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+pub mod git;
 pub mod gitlab;
 pub mod project;
--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,10 @@ use anyhow::{bail, Context, Result};
 use bytesize::ByteSize;
 use clap::Parser;
 use console::style;
-use git2::build::CheckoutBuilder;
-use git2::{Cred, ErrorCode, RemoteCallbacks};
+use gitlab_clone_all::git::*;
 use gitlab_clone_all::gitlab::*;
 use gitlab_clone_all::project::*;
-use std::cell::RefCell;
-use std::str::FromStr;
 use std::{path::Component, path::Path, path::PathBuf};
-use tokio::sync::mpsc::{Receiver, Sender};
-
-#[derive(Debug, PartialEq, Copy, Clone)]
-enum CloneMethod {
-    Ssh,
-    Https,
-}
-
-impl FromStr for CloneMethod {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "https" => Ok(CloneMethod::Https),
-            "ssh" => Ok(CloneMethod::Ssh),
-            _ => Err("no match"),
-        }
-    }
-}
 
 /// Clone all git repositories from gitlab
 #[derive(Parser)]
@@ -41,99 +19,6 @@ struct Opts {
     url: String,
     #[clap(short, long, possible_values = &["https","ssh"], default_value="https")]
     clone_method: CloneMethod,
-}
-
-async fn clone_projects(
-    mut rx_projects: Receiver<Project>,
-    tx_projects_actions: Sender<ProjectAction>,
-    expanded_path: &Path,
-    clone_method: CloneMethod,
-) -> Result<()> {
-    while let Some(project) = rx_projects.recv().await {
-        let path = expanded_path.join(&project.path_with_namespace);
-        log::debug!("Cloning project {:?} fs_path={:?}", &project, &path);
-
-        let tx_projects_actions = tx_projects_actions.clone();
-        tokio::spawn(async move {
-            let received_bytes = RefCell::new(0usize);
-            let received_objects = RefCell::new(0usize);
-
-            let mut builder = git2::build::RepoBuilder::new();
-            let mut callbacks = RemoteCallbacks::new();
-            callbacks.transfer_progress(|stats| {
-                received_bytes.replace(stats.received_bytes());
-                received_objects.replace(stats.received_objects());
-                true
-            });
-            let mut co = CheckoutBuilder::new();
-            co.progress(|path, cur, total| {
-                log::debug!("{:?} {}/{}", path, cur, total);
-            });
-            builder.with_checkout(co);
-            // Prepare fetch options.
-            let mut fo = git2::FetchOptions::new();
-
-            if clone_method == CloneMethod::Ssh {
-                callbacks.credentials(|_url, username_from_url, _allowed_types| {
-                    Cred::ssh_key(
-                        username_from_url.unwrap(),
-                        None,
-                        std::path::Path::new(&format!(
-                            "{}/.ssh/id_rsa_gitlab",
-                            std::env::var("HOME").unwrap()
-                        )),
-                        None,
-                    )
-                });
-            }
-            fo.remote_callbacks(callbacks);
-            builder.fetch_options(fo);
-
-            let url_to_repo = match clone_method {
-                CloneMethod::Ssh => &project.ssh_url_to_repo,
-                CloneMethod::Https => &project.http_url_to_repo,
-            };
-
-            match builder.clone(url_to_repo, &path) {
-                Ok(_repo) => {
-                    log::info!("Cloned project={:?}", &project);
-                    tx_projects_actions
-                        .try_send(ProjectAction::Cloned {
-                            project_path: project.path_with_namespace,
-                            received_bytes: received_bytes.take(),
-                            received_objects: received_objects.take(),
-                        })
-                        .with_context(|| "Failed to send ProjectCloned")
-                        .unwrap();
-                }
-                // Swallow this error
-                // TODO: Should we pull in that case?
-                Err(e) if e.code() == ErrorCode::Exists => {
-                    tx_projects_actions
-                        .try_send(ProjectAction::Cloned {
-                            project_path: project.path_with_namespace,
-                            received_bytes: received_bytes.take(),
-                            received_objects: received_objects.take(),
-                        })
-                        .with_context(|| "Failed to send ProjectCloned")
-                        .unwrap();
-                }
-                Err(e) => {
-                    log::error!("Failed to clone: project={:?} err={}", &project, e);
-                    tx_projects_actions
-                        .try_send(ProjectAction::Failed {
-                            project_path: project.path_with_namespace,
-                            err: e.to_string(),
-                        })
-                        .with_context(|| "Failed to send ProjectFailed")
-                        .unwrap();
-                }
-            };
-        });
-    }
-
-    log::debug!("Finished cloning");
-    Ok(())
 }
 
 // Needed to expand `~` which is otherwise understood literally as a relative path
